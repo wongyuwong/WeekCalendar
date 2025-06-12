75050a56324e5672392c83bf512fb99af63b366c
wongyuwong <68258928+wongyuwong@users.noreply.github.com>
2025-06-12 11:51:32 +0800

    Create demo.vue

---
+++ demo.vue
@@ -0,0 +1,35 @@
+<template>
+  <view>
+    <WeekCalendar 
+        yearMonth="2025-06"
+        theme="blue"
+        @backevent="weekCalendarChange"
+        @monthChange="onMonthChange"
+      />
+  </view>
+</template>
+
+<script lang="ts" setup>
+
+// 引入周选择日历组件
+import WeekCalendar from './WeekCalendar.vue'
+
+// 组件注册
+defineOptions({
+  components: {
+    WeekCalendar
+  }
+})
+
+const onMonthChange = (e: any) => {
+  console.log('月份变化:', e)
+}
+const weekCalendarChange = (e: any) => {
+  console.log('周选择器选择:', e)
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
